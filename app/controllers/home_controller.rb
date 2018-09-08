0730ab6bf980cd7395d5502d068b32cac98def29
aamir0688 <aamir_1388@hotmail.com>
2018-09-08 17:40:04 +0400

    ADD Internal Transfer to Account feature

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+	@@amount = 0
+ 	require 'httparty'
+  include HTTParty
   def index
   end
 
@@ -9,5 +12,24 @@ class HomeController < ApplicationController
     :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'  } )
   end
 
+  def new_internal_transfer_credit
 
+  end	
+
+  def internal_transfer_credit
+    @amount = @@amount
+    @url = "https://zcldrzy3bxoerecsf-mock.stoplight-proxy.io/internal_transfers/fidor_payin" 
+    @result = HTTParty.post(@url.to_str, 
+    :body => {
+              "account_id": "71616244",
+              "receiver": params[:receiver],
+              "external_uid": params[:external_uid],
+              "amount": params[:amount],
+              "subject": params[:subject]
+              }.to_json,
+    :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'  } )
+    respond_to do |format|
+      format.js { render :internal_transfer }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get 'home/index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "home/account_balance", to: 'home#account_balance', as: 'account_balance'
+
+	get "home/new_internal_transfer_credit", to: 'home#new_internal_transfer_credit', as: 'new_internal_transfer_credit'
+
+	post "home/internal_transfer_credit", to: 'home#internal_transfer_credit', as: 'internal_transfer_credit'
+
 end
