1fa75a79905db94a834b3e8d0b9590f6ad1e8b2d
aamir0688 <aamir_1388@hotmail.com>
2018-09-08 17:42:03 +0400

    ADD Internal Transfer debit feature

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,6 +12,27 @@ class HomeController < ApplicationController
     :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'  } )
   end
 
+  def new_internal_transfer_debit
+
+  end
+  def internal_transfer_debit
+    @amount = @@amount
+  	@url = "https://zcldrzy3bxoerecsf-mock.stoplight-proxy.io/internal_transfers/fidor_payout" 
+	  @result = HTTParty.post(@url.to_str, 
+    :body => {
+  						"account_id": "71616244",
+  						"receiver": params[:receiver],
+  						"external_uid": params[:external_uid],
+  						"amount": params[:amount],
+  						"subject": params[:subject]
+							}.to_json,
+    :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'  } )
+	  puts JSON.parse(@result.body)
+    respond_to do |format|
+      format.js { render :internal_transfer }
+    end
+  end	
+  
   def new_internal_transfer_credit
 
   end	
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
 
   get "home/account_balance", to: 'home#account_balance', as: 'account_balance'
 
+	get "home/new_internal_transfer_debit", to: 'home#new_internal_transfer_debit', as: 'new_internal_transfer_debit'
+
 	get "home/new_internal_transfer_credit", to: 'home#new_internal_transfer_credit', as: 'new_internal_transfer_credit'
+
+	post "home/internal_transfer_debit", to: 'home#internal_transfer_debit', as: 'internal_transfer_debit'
 
 	post "home/internal_transfer_credit", to: 'home#internal_transfer_credit', as: 'internal_transfer_credit'
 
