2c7a7b28be481bbeda5b0049a1fb493231b01c56
aamir0688 <aamir_1388@hotmail.com>
2018-09-08 17:37:28 +0400

    Send Request to Account Details Mock API

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def account_balance
+  	@url = "https://zcldrzy3bxoerecsf-mock.stoplight-proxy.io/accounts" 
+	  @result = HTTParty.get(@url.to_str, 
+    :body => "{}",
+    :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'  } )
+  end
+
+
 end
